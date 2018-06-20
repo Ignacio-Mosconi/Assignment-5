036c3df5fcf77299c95dfe67d3e657a2ea2e940c
Unknown <nacho7577@hotmail.com>
2018-06-20 18:28:33 -0300

    Added Clearing Functionality
    - The listbox can now be cleared.

---
+++ Assignment-5/Definitions.h
@@ -0,0 +1,7 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+#endif
--- Assignment-5/Main.cpp
+++ Assignment-5/Main.cpp
@@ -5,6 +5,7 @@
 #include <nana/gui/widgets/listbox.hpp>
 #include <nana/gui/widgets/button.hpp>
 #include <HS.h>
+#include "Definitions.h"
 
 int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char*, int nShowCmd)
 {
@@ -12,47 +13,51 @@ int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char*, int n
 	using namespace HS;
 
 	HighScore* leaderboard = new HighScore();
-	leaderboard->addHighScore(30, "Peter");
+	leaderboard->addHighScore(30, "Ross");
 	leaderboard->addHighScore(50, "Monica");
-	leaderboard->addHighScore(40, "Chandler");
+	leaderboard->addHighScore(75, "Chandler");
 	leaderboard->addHighScore(70, "Rachel");
+	leaderboard->addHighScore(25, "Phoebe");
+	leaderboard->addHighScore(40, "Joey");
 
-	form leaderboardForm { rectangle(300, 200, 640, 480), appearance(true, true, true, false, true, false, false) };
+	form leaderboardForm(API::make_center(WINDOW_WIDTH, WINDOW_HEIGHT), appear::decorate<>());
+	leaderboardForm.caption("Leaderboards - Powered by Nana & HS");
 
-	label title { leaderboardForm, "<bold color = 0x177717 size = 24 center> Highest Scores</>" };
+	label title(leaderboardForm, "<bold color = 0x177717 size = 24 center> Highest Scores</>" );
 	title.text_align(align::center);
 	title.format(true);
 
-	listbox scores{ leaderboardForm };
+	listbox scores(leaderboardForm);
 
 	scores.append_header("Name");
 	scores.append_header("Score");
 
 	string name;
 	int score;
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < 5; i++)
 	{
 		leaderboard->showHighScore(i, score, name);
 		scores.at(0).append({ name, to_string(score)});
 	}
 
-	button quitButton { leaderboardForm, "Quit" };
+	button quitButton(leaderboardForm, "Quit");
 	quitButton.events().click([&leaderboardForm] {
 		leaderboardForm.close();
 	});
 
-	button clearButton{ leaderboardForm, "Clear" };
-	clearButton.events().click([leaderboard] {
+	button clearButton(leaderboardForm, "Clear");
+	clearButton.events().click([leaderboard, &scores] {
 		leaderboard->clearScore();
+		scores.clear();
 	});
 
-	place formPlace { leaderboardForm };
+	place layout(leaderboardForm);
 
-	formPlace.div("vertical <title> <weight = 70% scores> <weight = 10% margin = [10, 10] gap = 15 buttons>");
-	formPlace["title"] << title;
-	formPlace["scores"] << scores;
-	formPlace["buttons"] << clearButton << quitButton;
-	formPlace.collocate();
+	layout.div("vertical <title> <weight = 70% scores> <weight = 10% margin = [10, 10] gap = 15 buttons>");
+	layout["title"] << title;
+	layout["scores"] << scores;
+	layout["buttons"] << clearButton << quitButton;
+	layout.collocate();
 
 	leaderboardForm.show();
 
