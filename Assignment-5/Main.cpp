41e94c738cbdde36bef482db87515c2d8ebc593a
Unknown <nacho7577@hotmail.com>
2018-06-20 16:59:14 -0300

    Added a "Listbox"
    - After hours of research, a way to display the scores properly has been found: the "listbox".

--- Assignment-5/Main.cpp
+++ Assignment-5/Main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <nana/gui.hpp>
 #include <nana/gui/widgets/label.hpp>
+#include <nana/gui/widgets/listbox.hpp>
 #include <nana/gui/widgets/button.hpp>
 #include <HS.h>
 
@@ -16,26 +17,43 @@ int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char*, int n
 	leaderboard->addHighScore(40, "Chandler");
 	leaderboard->addHighScore(70, "Rachel");
 
-	int highestScore;
-	string bestPlayer;
-	leaderboard->showHighScore(0, highestScore, bestPlayer);
+	form leaderboardForm;
 
-	form fm;
-	
-	label lab{ fm, "Highest Score: " + bestPlayer };
-	lab.format(true);
+	label title { leaderboardForm, "<bold green size = 16 center> Highests Scores</>" };
+	title.text_align(align::center);
+	title.format(true);
 
-	button btn{ fm, "Quit" };
-	btn.events().click([&fm] {
-		fm.close();
+	listbox scores{ leaderboardForm };
+	scores.append_header("Name");
+	scores.append_header("Score");
+
+	string name;
+	int score;
+	for (int i = 0; i < 4; i++)
+	{
+		leaderboard->showHighScore(i, score, name);
+		scores.at(0).append({ name, to_string(score)});
+	}
+
+	button quitButton { leaderboardForm, "Quit" };
+	quitButton.events().click([&leaderboardForm] {
+		leaderboardForm.close();
 	});
 
-	fm.div("vert <><<><weight=80% text><>><><weight=24<><button><>><>");
-	fm["text"] << lab;
-	fm["button"] << btn;
-	fm.collocate();
+	button clearButton{ leaderboardForm, "Clear" };
+	clearButton.events().click([leaderboard] {
+		leaderboard->clearScore();
+	});
 
-	fm.show();
+	place formPlace { leaderboardForm };
+
+	formPlace.div("vertical <title> <weight = 50% scores> <margin = 10 gap = 15 buttons>");
+	formPlace["title"] << title;
+	formPlace["scores"] << scores;
+	formPlace["buttons"] << clearButton << quitButton;
+	formPlace.collocate();
+
+	leaderboardForm.show();
 
 	exec();
 }
